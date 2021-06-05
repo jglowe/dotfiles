17b3b5b095108749cf206809541e48e111fd6613
Jonathan Lowe <me@jonathanlowe.dev>
2021-06-05 14:00:23 -0400

    Added support for fzf in manjaro

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,23 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+	export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+	# Auto-completion
+	# ---------------
+	[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.bash" 2> /dev/null
+
+	# Key bindings
+	# ------------
+	source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.bash" 2> /dev/null
+if [[ ! "$PATH" == */usr/share/fzf* ]]; then
+	export PATH="${PATH:+${PATH}:}/usr/share/fzf"
+	# Auto-completion
+	# ---------------
+	[[ $- == *i* ]] && source "/usr/share/fzf/completion.bash" 2> /dev/null
 
-# Key bindings
-# ------------
-source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
+	# Key bindings
+	# ------------
+	source "/usr/share/fzf/key-bindings.bash"
+fi
