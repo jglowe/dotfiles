d0eb24b145554454576dc2ec8b3b4285f2b74eb7
Jonathan Lowe <me@jonathanlowe.dev>
2022-01-14 20:36:46 -0500

    Fixed FZF settings

--- .fzf.bash
+++ .fzf.bash
@@ -1,23 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]] && [ -d /opt/homebrew/opt/fzf/bin ]; then
-	export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
-	# Auto-completion
-	# ---------------
-	[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.bash" 2> /dev/null
-
-	# Key bindings
-	# ------------
-	source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
 fi
 
-if [[ ! "$PATH" == */usr/share/fzf* ]] && [ -d /usr/share/fzf ]; then
-	export PATH="${PATH:+${PATH}:}/usr/share/fzf"
-	# Auto-completion
-	# ---------------
-	[[ $- == *i* ]] && source "/usr/share/fzf/completion.bash" 2> /dev/null
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.bash" 2> /dev/null
 
-	# Key bindings
-	# ------------
-	source "/usr/share/fzf/key-bindings.bash"
-fi
+# Key bindings
+# ------------
+source "/opt/homebrew/opt/fzf/shell/key-bindings.bash"
