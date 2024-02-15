dac8d4ebe3ce6937293afb1c575fea579af1fdc7
Jonathan Lowe <me@jonathanlowe.dev>
2024-02-14 23:46:49 -0500

    Added wezterm support

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require('wezterm')
+
+local config = wezterm.config_builder()
+
+config.hide_tab_bar_if_only_one_tab = true
+config.font = wezterm.font 'MesloLGS Nerd Font Mono'
+config.color_scheme = 'Classic Dark (base16)'
+
+return config
